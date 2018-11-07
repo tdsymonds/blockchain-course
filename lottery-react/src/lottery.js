ee1c8ca6191479277e0ed7cee01778e3ada4b3b9
Tom Symonds <tdsymonds@hotmail.com>
2018-11-07 11:33:40 +0000

    Add lottery react front end

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from "./web3";
+
+const address = '0x5BabE7B061B72ebd161BcfD5e0599fD0430aE488';
+console.log(address);
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
